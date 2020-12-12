a0fa52c93cfeae85fc3333808b7cc6b83d99adbf
SorinAlbu <albusorin28@gmail.com>
2020-12-12 14:38:10 +0200

    response Pokemon

---
+++ src/main/java/com/example/biblePoke/model/ResponseBodyPokemon.java
@@ -0,0 +1,13 @@
+package com.example.biblePoke.model;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ResponseBodyPokemon {
+    private String count;
+    private String next;
+    private String previous;
+    private List<Pokemon> results;
+}
