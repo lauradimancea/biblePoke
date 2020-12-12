7dbf8a304a2c9fd9c62125407808bbe411159a8d
Laura Dimancea <laura_dimancea@smart-x.net>
2020-12-12 10:08:13 +0200

    structure

---
+++ src/test/java/com/example/biblePoke/BiblePokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biblePoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblePokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
